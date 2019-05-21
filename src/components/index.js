a8a54e50f5e7ec155ddc1cea610a9a1c1d91dfeb
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-21 23:51:07 +0700

    Clean project (#2)

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
+// eslint-disable-next-line import/prefer-default-export
 export { default as Repos } from './Repos';
-export { default as Navbar } from './Navbar';
-export { default as NotFound } from './NotFound';
