8e6c2f0bad74f9bf35a9324cb865b5b049eb9af6
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-22 00:29:54 +0700

    Complete UI

--- src/services/reposService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export function* fetchRepos() {
-  const response = yield axios.get('https://api.github.com/users/cuongw/repos');
-  return response.data ? response.data: [];
-};
