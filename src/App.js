6a252c38cc0b1a5a2b8309dc0903751313c02ad4
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-21 23:49:02 +0700

    Clean project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ReposContainer } from './containers';
-import { Navbar, NotFound } from './components';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route path="/" exact component={ReposContainer} />
-          <Route path="*" component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
-  );
+  return <ReposContainer />;
 }
 
 export default App;
