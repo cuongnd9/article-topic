0dcb220a3d6714e965800289c68c2fa2151b9b23
Thien Chi Vi <meomeocf98@gmail.com>
2019-05-25 01:39:30 +0700

    Connect api (#6)

--- server/article_topic/settings.py
+++ server/article_topic/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -50,7 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'article_topic.urls'
-
+CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Editor, EditorState } from 'draft-js';
-import { ClipLoader } from 'react-spinners';
+import React, { useState, useEffect, useRef } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Editor, EditorState } from "draft-js";
+import { ClipLoader } from "react-spinners";
 
 function App() {
   const [topics, setTopics] = useState([]);
@@ -19,11 +19,30 @@ function App() {
     setTopics([]);
     // Set fetch data status.
     setIsFetchData(true);
+    // Get current text.
+    const currentText = editorState.getCurrentContent().getPlainText();
+    // POST params.
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: currentText
+      })
+    };
     // Fecth data from api.
-    fetch('https://api.github.com/users/cuongw/repos')
+    fetch("http://localhost:8000/article/", params)
       .then(res => res.json())
-      .then(data => setTopics(data))
-      .catch(_ => setTopics([]));
+      .then(data => {
+        const { result } = JSON.parse(data.replace(/\'/g, '"'));
+        setTopics(result);
+        setIsFetchData(false);
+      })
+      .catch(_ => {
+        setTopics([]);
+        setIsFetchData(false);
+      });
   }
 
   useEffect(() => {
@@ -33,7 +52,11 @@ function App() {
   return (
     <div className="container p-5">
       <h1 className="text-success">Article Topic</h1>
-      <div className="border rounded p-5 my-5" onClick={focusEditor}>
+      <div
+        className="border p-5 my-5"
+        style={{ borderRadius: "12px" }}
+        onClick={focusEditor}
+      >
         <Editor
           ref={editor}
           editorState={editorState}
@@ -42,24 +65,37 @@ function App() {
       </div>
       <input
         className="form-control btn btn-success d-inline mb-5"
+        style={{ borderRadius: "12px" }}
         type="button"
         value="Suggest a topic"
         onClick={fetchData}
       />
-      {isFetchData && !topics.length && (
-        <p className="text-center">
-          <ClipLoader sizeUnit={'px'} size={40} color={'#123abc'} />
-        </p>
+      {isFetchData && topics && !topics.length && (
+        <div className="text-center">
+          <ClipLoader sizeUnit={"px"} size={40} color={"#123abc"} />
+        </div>
       )}
-      {topics.length > 0 &&
+      {topics && !topics.length && (
+        <p className="text-center text-danger h4">Nothing</p>
+      )}
+      {topics && topics.length > 0 && (
+        <div className="border-bottom d-flex mb-5">
+          <p className="h4 text-primary w-100 ml-5">Topic</p>
+          <p className="h4 text-primary mr-5">Rate</p>
+        </div>
+      )}
+      {topics &&
+        topics.length > 0 &&
         topics.map((topic, index) => (
           <div className="border-bottom d-flex mb-5" key={index}>
-            <p className="h4 text-primary w-100 ml-5">Topic</p>
-            <p className="badge badge-danger mr-5">50%</p>
+            <p className="h4 text-info w-100 ml-5">{topic.topic}</p>
+            <p className="badge badge-danger mr-5">{topic.rate}</p>
           </div>
         ))}
-      <p className="text-secondary text-center">
-        Made with ⌨️ and 🙌
+      <p className="text-secondary text-center mt-5">
+        <span role="img" aria-label="emoji">
+          Made with ⌨️ and 🙌
+        </span>
         <br />
         <small className="text-secondary">© 2019 cuongw</small>
       </p>
